c376525ad939322e7ffb1aa93e88cfa61ff853b0
Crowdbotics <team@crowdbotics.com>
2020-09-26 18:15:10 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen15112112', human_name: 'Blank Screen 15', access_route: 'BlankScreen15112112'},
 { name: 'ArticleList112080', human_name: 'Article List', access_route: 'ArticleList112080'},
 { name: 'ArticleList112079', human_name: 'Article List', access_route: 'ArticleList112079'},
 { name: 'ArticleList112078', human_name: 'Article List', access_route: 'ArticleList112078'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen15112112Navigator from '../features/BlankScreen15112112/navigator';
 import ArticleList112080Navigator from '../features/ArticleList112080/navigator';
 import ArticleList112079Navigator from '../features/ArticleList112079/navigator';
 import ArticleList112078Navigator from '../features/ArticleList112078/navigator';
@@ -19,6 +20,7 @@ import ArticleList112059Navigator from '../features/ArticleList112059/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+BlankScreen15112112: { screen: BlankScreen15112112Navigator },
 ArticleList112080: { screen: ArticleList112080Navigator },
 ArticleList112079: { screen: ArticleList112079Navigator },
 ArticleList112078: { screen: ArticleList112078Navigator },
